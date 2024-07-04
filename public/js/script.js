2a5c7add36a2b872c69a92717feeaf5fd1aa8db8
winterrr03 <dong.huynhquang21503@gmail.com>
2024-07-04 11:09:55 +0700

    feat(tour): add tour

--- public/js/script.js
+++ public/js/script.js
@@ -7,3 +7,73 @@ if (sliderMain) {
     },
   });
 }
+
+// alert-add-cart-susscess
+const alertAddCartSusscess = () => {
+  const elementAlert = document.querySelector("[alert-add-cart-susscess]");
+  if (elementAlert) {
+    elementAlert.classList.remove("alert-hidden");
+
+    setTimeout(() => {
+      elementAlert.classList.add("alert-hidden");
+    }, 3000);
+
+    const closeAlert = elementAlert.querySelector("[close-alert]");
+    closeAlert.addEventListener("click", () => {
+      elementAlert.classList.add("alert-hidden");
+    });
+  }
+};
+// End alert-add-cart-susscess
+
+// Hiển thị số lượng vào mini cart
+const showMiniCart = () => {
+  const miniCart = document.querySelector("[mini-cart]");
+  if (miniCart) {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    miniCart.innerHTML = cart.length;
+  }
+};
+showMiniCart();
+// Hết Hiển thị số lượng vào mini cart
+
+// Cart
+// Khởi tạo giỏ hàng
+const cart = localStorage.getItem("cart");
+if (!cart) {
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+
+// Thêm tour vào giỏ hàng
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if (formAddToCart) {
+  formAddToCart.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const quantity = parseInt(formAddToCart.quantity.value);
+    const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+
+    if (quantity > 0 && tourId) {
+      const cart = JSON.parse(localStorage.getItem("cart"));
+
+      const indexExistTour = cart.findIndex((item) => item.tourId == tourId);
+
+      if (indexExistTour == -1) {
+        cart.push({
+          tourId: tourId,
+          quantity: quantity,
+        });
+      } else {
+        cart[indexExistTour].quantity =
+          cart[indexExistTour].quantity + quantity;
+      }
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+
+      showMiniCart();
+
+      alertAddCartSusscess();
+    }
+  });
+}
+// End Cart
